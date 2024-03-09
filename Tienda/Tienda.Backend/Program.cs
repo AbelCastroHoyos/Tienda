bb280c4dcdcef968bcf377b5938fe748370834e8
Abel Castro <abelcastrohoyos@yahoo.es>
2024-03-09 16:42:39 -0500

    Create DataBase and first entity Country

--- Tienda/Tienda.Backend/Program.cs
+++ Tienda/Tienda.Backend/Program.cs
@@ -1,3 +1,8 @@
+//using Microsoft.AspNetCore.Localization;
+using Microsoft.EntityFrameworkCore;
+//using System.Globalization;
+using Tienda.Backend.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,8 +11,20 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer("name=localConnection"));
+
+// Agrega estas líneas dentro del método Main:
+//builder.Services.AddLocalization();
+
+// ... (resto de tu código)
 
 var app = builder.Build();
+
+//app.UseRequestLocalization(options =>
+//{
+//    options.DefaultRequestCulture = new RequestCulture("es-CO"); // Establece la cultura predeterminada a español de Colombia
+//    options.SupportedCultures = CultureInfo.GetCultures(CultureTypes.AllCultures).ToList();
+//});
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
--- Tienda/Tienda.Shared/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Tienda.Shared
-{
-    public class Class1
-    {
-
-    }
-}
