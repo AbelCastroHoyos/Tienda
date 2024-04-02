509a589cbd9d0f5c14e5d9ac5550334103547ae5
Abel Castro <abelcastrohoyos@yahoo.es>
2024-04-01 21:56:12 -0500

    Creación de API Countries

---
+++ Tienda/Tienda.Backend/Controllers/CountriesController.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Tienda.Backend.Data;
+using Tienda.Shared.Entities;
+
+namespace Tienda.Backend.Controllers
+{
+    [ApiController]
+    [Route("API/[Controller]")]
+    public class CountriesController: ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public CountriesController(DataContext context) 
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAsync() 
+        { 
+            return Ok(await _context.Countries.ToListAsync());        
+        }
+
+        /// <summary>
+        /// Método para obtener la información de un país por su ID.
+        /// </summary>
+        /// <remarks>
+        /// <b>Este método es muy útil cuando se requiere tener info de un solo país.</b>
+        /// &#xD;Si no se suministra un ID o este no existe
+        /// &#xD;se recibe una respuesta not found (404).
+        /// &#xD;El ID debe ser un valor entero. 
+        /// </remarks>
+        /// <param name="id">ID del país</param>
+        /// <returns>El país si este fue encontrado.</returns>
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(typeof(Country), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> GetAsync(int id) 
+        {
+            var country = await _context.Countries.FindAsync(id);
+            if (country == null)
+            {
+                return NotFound();
+            }
+            return Ok(country);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PostAsync(Country country)
+        {
+            _context.Add(country);
+            await _context.SaveChangesAsync();
+            return Ok(country);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> PutAsync(Country country)
+        {
+            _context.Update(country);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAsync(int id)
+        {
+            var country = await _context.Countries.FindAsync(id);
+            if (country == null)
+            {
+                return NotFound();
+            }
+            _context.Remove(country);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+    }
+}
--- Tienda/Tienda.Backend/Program.cs
+++ Tienda/Tienda.Backend/Program.cs
@@ -1,36 +1,44 @@
-//using Microsoft.AspNetCore.Localization;
 using Microsoft.EntityFrameworkCore;
-//using System.Globalization;
+using System.Reflection;
 using Tienda.Backend.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Title = "Mi API",
+        Version = "v1",
+        Description = "Descripción de mi API" // Opcional: Puedes agregar una descripción
+    });
+
+    // Configura el documento Swagger para que incluya los comentarios XML
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    c.IncludeXmlComments(xmlPath);
+});
 builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer("name=localConnection"));
-
-// Agrega estas líneas dentro del método Main:
-//builder.Services.AddLocalization();
-
-// ... (resto de tu código)
 
 var app = builder.Build();
 
-//app.UseRequestLocalization(options =>
-//{
-//    options.DefaultRequestCulture = new RequestCulture("es-CO"); // Establece la cultura predeterminada a español de Colombia
-//    options.SupportedCultures = CultureInfo.GetCultures(CultureTypes.AllCultures).ToList();
-//});
+app.UseCors(x => x
+    .AllowAnyMethod()
+    .AllowAnyHeader()
+    .SetIsOriginAllowed(origin => true)
+    .AllowCredentials());
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c => 
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+    });
 }
 
 app.UseHttpsRedirection();
