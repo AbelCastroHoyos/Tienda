c572126aabd591c1eb83e2fc1e1d4e639eaf2e98
Abel Castro <abelcastrohoyos@yahoo.es>
2024-04-03 18:34:38 -0500

    Begin repository pattern

---
+++ Tienda/Tienda.Frontend/Repositories/IRepository.cs
@@ -0,0 +1,14 @@
+﻿namespace Tienda.Frontend.Repositories
+{
+    public interface IRepository
+    {
+        Task<HttpResponseWrapper<T>> GetAsync<T>(string url);
+
+        // Este Post no devuelve respuesta
+        Task<HttpResponseWrapper<object>> PostAsync<T>(string url, T model);
+
+        // Este Post devuelve respuesta
+        Task<HttpResponseWrapper<TActionResponse>> PostAsync<T, TActionResponse>(string url, T model);
+
+    }
+}
